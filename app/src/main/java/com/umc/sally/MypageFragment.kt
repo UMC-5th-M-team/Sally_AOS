60b2db45f61ae7a137c297f3b6f6ab245355abb9
jung0115 <jungim7490@gmail.com>
2024-01-05 01:54:59 +0900

    [FEAT] #2 - 마이페이지 > 돌아보기, 읽은 책 통계 상단 부분 완료

--- app/src/main/java/com/umc/sally/MypageFragment.kt
+++ app/src/main/java/com/umc/sally/MypageFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.umc.sally.databinding.FragmentMypageBinding
 
 
@@ -21,6 +23,28 @@ class MypageFragment : Fragment() {
         _binding = FragmentMypageBinding.inflate(layoutInflater)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // status bar
+        val mWindow = requireActivity().window
+        mWindow.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        mWindow.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        mWindow.statusBarColor = ContextCompat.getColor(requireActivity(), R.color.main_color)
+
+        // 읽은 책 통계
+        binding.btnMypageMove1.setOnClickListener {
+            binding.relativeMypage1.visibility = View.GONE
+            binding.relativeMypage2.visibility = View.VISIBLE
+        }
+
+        // 돌아보기
+        binding.btnMypageMove2.setOnClickListener {
+            binding.relativeMypage1.visibility = View.VISIBLE
+            binding.relativeMypage2.visibility = View.GONE
+        }
     }
 
     override fun onDestroy() {
