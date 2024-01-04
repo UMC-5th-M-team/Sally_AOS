2cda656e5cfcd188305c6842581af1ef87803331
jung0115 <jungim7490@gmail.com>
2024-01-05 06:06:16 +0900

    [FEAT] #1 - 책장

--- app/src/main/java/com/umc/sally/ShelfBookFragment.kt
+++ app/src/main/java/com/umc/sally/ShelfBookFragment.kt
@@ -22,6 +22,12 @@ class ShelfBookFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
