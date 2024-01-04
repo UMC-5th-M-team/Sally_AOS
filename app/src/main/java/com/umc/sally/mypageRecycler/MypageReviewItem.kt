544e001fb33f1b7a414c6d4c8e8e2d6e20e0695f
jung0115 <jungim7490@gmail.com>
2024-01-05 03:15:48 +0900

    [FEAT] #2 - 마이페이지 > 돌아보기 Review Recyclerview

---
+++ app/src/main/java/com/umc/sally/mypageRecycler/MypageReviewItem.kt
@@ -0,0 +1,11 @@
+package com.umc.sally.mypageRecycler
+
+data class MypageReviewItem(
+    val bookTitle: String,
+    val bookWriter: String,
+    val reviewTitle: String,
+    val reviewContent: String,
+    val imageUrl: String,
+    val date: String,
+    val page: String
+)
