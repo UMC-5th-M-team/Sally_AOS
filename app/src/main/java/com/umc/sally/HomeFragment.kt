4101b39ba35f0bfef0cf96f2bfbece4abac74b7f
jung0115 <jungim7490@gmail.com>
2024-01-05 06:57:06 +0900

    [MERGE] 충돌 해결 중

--- app/src/main/java/com/umc/sally/HomeFragment.kt
+++ app/src/main/java/com/umc/sally/HomeFragment.kt
@@ -1,9 +1,7 @@
 package com.umc.sally
 
-<<<<<<< HEAD
 import android.content.Intent
-=======
->>>>>>> 0edb57f ([FEAT] 하단바 생성)
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -23,7 +21,7 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeBinding.inflate(layoutInflater)
 
-<<<<<<< HEAD
+
         val intent = Intent(requireContext(), SearchActivity::class.java)
         val intent2 = Intent(requireContext(), NewBookActivity::class.java)
 
@@ -36,8 +34,7 @@ class HomeFragment : Fragment() {
             val cal_month = binding.calMonth.toString()
         }
 
-=======
->>>>>>> 0edb57f ([FEAT] 하단바 생성)
+
         return binding.root
     }
 
